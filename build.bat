d02ed2beb006a72343b5a1c26a47788bab0bcdf6
belsrc <bryan@bryanckizer.com>
2013-12-19 16:00:11 -0500

    changed build back

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-REM call vendor\bin\sami.php.bat update doc-config.php
-REM call vendor\bin\pdepend.bat --summary-xml=docs\phpdepend\summary.xml --jdepend-chart=docs\phpdepend\jdepend.svg --overview-pyramid=docs\phpdepend\pyramid.svg src
-REM echo Checking for copy paste
-REM call vendor\bin\phpcpd.bat src > docs\copypaste.txt
+call vendor\bin\sami.php.bat update doc-config.php
+call vendor\bin\pdepend.bat --summary-xml=docs\phpdepend\summary.xml --jdepend-chart=docs\phpdepend\jdepend.svg --overview-pyramid=docs\phpdepend\pyramid.svg src
+echo Checking for copy paste
+call vendor\bin\phpcpd.bat src > docs\copypaste.txt
 call vendor\bin\phpunit.bat --verbose --coverage-html docs\phpunit\coverage --testdox-html docs\phpunit\dox.html
